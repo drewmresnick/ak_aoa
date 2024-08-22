a0b2b9eac6f911c556ccd854a8f20aed804c8997
Breanna Xiong <breanna.xiong@noaa.gov>
2024-08-21 21:32:20 -0400

    reupload: fix gihub repo, update code from sf::intersect to rmapshaper::ms_clip, and clip to hex grid

--- code/24_sfpf_offshore_seafood_processors.R
+++ code/24_sfpf_offshore_seafood_processors.R
@@ -1,6 +1,6 @@
-########################################################################################################
-### 24. Alaska DEC Seafood Processing Facilities: AKG5253000 Offshore Seafood Processors,	ak_cs_030  ###
-########################################################################################################
+##########################################################################################################################
+### 24. Alaska DEC Seafood Processing Facilities: AKG5253000 Offshore Seafood Processors Permitted Vessels,	ak_cs_030  ###
+##########################################################################################################################
 
 
 # clear environment
@@ -99,67 +99,102 @@ code_dict <-list(study_area = c("cordova",
 
 #####################################
 #####################################
-# This section cleans the data and "clips" the data to our area of interest. 
-
+# This section cleans the data and clips the data to our area of interest. 
 
 ## use the length of the code_dict "study_area" element to decide how many iterations are needed 
 for(i in 1:length(code_dict$study_area)){
   
-  # call the study area dictionary for the iteration
-  dict_study <- code_dict$study_area[[i]]
-  
-  # call the corresponding study area code 
-  dict_code <- code_dict$code[[i]]
-  
-  # call in the dissolved hex grids
-  ## As there are two layers in the geopackage with the study area name, 
-  ## it's essential to read out the full name to avoid selecting the incorrect one.
-  dataset <- paste(dict_study, "fiveacre_dissolve", sep = "_")
-  
-  ## path to the dissolved hex grids 
-  hex_path <- paste(study_area_dir, "/", dict_study, "/b_intermediate_data/", dict_study, "_study_area.gpkg", sep = "")
-  
-  ## for each item in the code_dict list, search the study area geopackage for the hex layer corresponding to that item
-  # buffer by 500 m (this is computationally faster than buffering the dataset first and then intersecting)
-  hex_by_study <- sf::st_read(dsn = hex_path,
-                              layer = sf::st_layers(hex_path)[[1]][grep(pattern = dataset,
-                                                                        sf::st_layers(dsn = hex_path)[[1]])]) %>% sf::st_buffer(dist = 500)
-  
-  
-  # function to clean data
-  clean_data <- function(raw_data){
-    data_layer <- raw_data %>%
-      # reproject the same coordinate reference system (crs) as the study area
-      sf::st_transform("ESRI:102008") %>% # EPSG WKID 102008 (https://epsg.io/102008)
-      # obtain data within buffered hexed study area
-      sf::st_intersection(hex_by_study) %>%
-      # add a setback (buffer) distance of 500m
-      sf::st_buffer(dist = 500) 
-    return(data_layer)
-  }
-  
-  # Load Alaska DEC Seafood Processing Facilities: AKG523000 Offshore Seafood Processors Permitted Vessel
-  ## Source: https://dec.alaska.gov/arcgis/rest/services/Water/Seafood_Processing/MapServer/6
-  ### data
-  read_data <- sf::st_read(dsn = raw_constraints_gpkg, layer = uniqueID) %>% clean_data()
-  
-  # name split by the "_"
-  id <- strsplit(uniqueID, "_(?!.*_)", perl=TRUE)[[1]]
-  
-  # create a new unique code that's specific to the study area, not just the region
-  out_code <- paste(region, dict_code, submodel, id[2], sep="_")
-  
-  
-  # Export data
-  ## intermediate geopackage directory
-  intermediate_gpkg <- paste(study_area_dir, dict_study, "b_intermediate_data/constraints/constraints.gpkg", sep = "/")
-  
-  ## run the export tool
-  sf::st_write(obj = read_data, dsn = intermediate_gpkg, out_code , append = F)
+  # Error catching
+  tryCatch({
+    
+    # call the study area dictionary for the iteration
+    dict_study <- code_dict$study_area[[i]]
+    
+    # call the corresponding study area code
+    dict_code <- code_dict$code[[i]]
+    
+    # call in the dissolved hex grids
+    ## As there are two layers in the geopackage with the study area name,
+    ## it's essential to read out the full name to avoid selecting the incorrect one.
+    dataset <- paste(dict_study, "fiveacre_dissolve", sep = "_")
+    
+    ## path to the dissolved hex grids
+    hex_path <- paste(study_area_dir, "/", dict_study, "/b_intermediate_data/", dict_study, "_study_area.gpkg", sep = "")
+    
+    ## for each item in the code_dict list, search the study area geopackage for the dataset layer corresponding to that item
+    hex_by_study <- sf::st_read(dsn = hex_path,
+                                layer = sf::st_layers(hex_path)[[1]][grep(pattern = dataset,
+                                                                          sf::st_layers(dsn = hex_path)[[1]])])
+    
+    # function to clean data
+    clean_data <- function(raw_data){
+      data_layer <- raw_data %>%
+        # reproject the same coordinate reference system (crs) as the study area
+        sf::st_transform("ESRI:102008") %>% # EPSG WKID 102008 (https://epsg.io/102008)
+        # obtain data within buffered hexed study area
+        rmapshaper::ms_clip(hex_by_study) %>%
+        # create field called "layer" and fill with "seafood processors permit vessels" for summary
+        dplyr::mutate(layer = "seafood processors permit vessels")
+      return(data_layer)
+    }
+    
+    # Load Alaska DEC Seafood Processing Facilities: AKG523000 Offshore Seafood Processors Permitted Vessel
+    ## Source: https://dec.alaska.gov/arcgis/rest/services/Water/Seafood_Processing/MapServer/6
+    ### data
+    read_data <- sf::st_read(dsn = raw_constraints_gpkg, layer = uniqueID) %>% clean_data
+    
+    
+    # name split by the "_"
+    id <- strsplit(uniqueID, "_(?!.*_)", perl=TRUE)[[1]]
+    
+    # create a new unique code that's specific to the study area, not just the region
+    out_code <- paste(region, dict_code, submodel, id[2], sep="_")
+    
+    
+    # Export data
+    ## intermediate geopackage directory
+    intermediate_gpkg <- paste(study_area_dir, dict_study, "b_intermediate_data/constraints/constraints.gpkg", sep = "/")
+    
+    
+    ## run the export tool
+    sf::st_write(obj = read_data, dsn = intermediate_gpkg, out_code , append = F)
+    
+    #create the unique hex layer name
+    hex_layer <- paste(dict_study, "_fiveacre", sep = "")
+    
+    #read in the study area hex layer
+    region_hex <- sf::st_read(dsn = hex_path,
+                              layer = hex_layer)
+    
+    # turn clipped data into hex grid
+    # find where data intersect with hex grids
+    region_data_hex_function <- function(region_hex, read_data){
+      region_data_hex <- region_hex[read_data, ] %>%
+        # spatially join  values to study area hex cells
+        sf::st_join(x = .,
+                    y = read_data,
+                    join = st_intersects) %>%
+        # select fields of importance
+        dplyr::select(GRID_ID)
+      
+      return(region_data_hex)
+    }
+    
+    # run the function
+    hex_read_data <- region_data_hex_function(region_hex, read_data)
+    
+    # submodel geopackage directory
+    submodel_gpkg <- paste(study_area_dir, dict_study, "c_submodel_data/constraints/constraints.gpkg", sep = "/")
+    
+    # run the export tool
+    sf::st_write(obj = hex_read_data, dsn = submodel_gpkg, out_code , append = F)
+    
+    # Error catching  
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
 }
 
-#####################################
-#####################################
 
+#####################################
+#####################################
 # calculate end time and print time difference
 print(Sys.time() - start) # print how long it takes to calculate
