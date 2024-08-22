0848b405526dcda1ce0f87a5cc4c6c7b3096710d
Breanna Xiong <breanna.xiong@noaa.gov>
2024-08-22 13:40:19 -0400

    All the study areas bathymetry used to be ak_cs_054, but the new script gives all the study area bathymetry their own unique ID (ak_cs_054 - ak_cs_064). The bathymetry in the submodel data are all unique IDs as well, separated by study area.

--- code/41_bathymetry.R
+++ code/41_bathymetry.R
@@ -1,5 +1,5 @@
 ################################
-### 41. Bathymetry  ###
+### 41. Bathymetry           ###
 ################################
 
 # clear environment
@@ -20,14 +20,15 @@ pacman::p_load(docxtractr, dplyr, elsa, fasterize, fs, ggplot2, janitor, ncf, pa
 #####################################
 
 
-bathymetry_path <- "C:/Users/Eliza.Carter/Documents/Projects/ak_aoa/data/extracted_bathymetry_hex_grids.gpkg"
-output_base_path <- "C:/Users/Eliza.Carter/Documents/Projects/ak_aoa/study_area/study_area"
+bathymetry_path <- "C:/Users/Breanna.Xiong/Documents/R Scripts/ak_aoa/data/aa_exploration_data/extracted_bathymetry_hex_grids.gpkg"
+output_base_path <- "C:/Users/Breanna.Xiong/Documents/R Scripts/ak_aoa/study_area"
 
 
 
 code_dict <- list(
   study_area = c("cordova", "craig", "juneau", "ketchikan", "kodiak", "metlakatla", "petersburg", "seward", "sitka", "valdez", "wrangell"),
-  code = c("co", "cr", "ju", "ke", "ko", "me", "pe", "se", "si", "va", "wr")
+  code = c("co", "cr", "ju", "ke", "ko", "me", "pe", "se", "si", "va", "wr"),
+  id_num = c("cs_054", "cs_055", "cs_056", "cs_057", "cs_058", "cs_059", "cs_060", "cs_061", "cs_062", "cs_063", "cs_064")
 )
 
 study_area_layers <- sf::st_layers(bathymetry_path)$name
@@ -50,29 +51,38 @@ for (layer in study_area_layers) {
   
   # Update "depth_range_suspended" scenario
   data$depth_range_suspended <- ifelse(data$depth_range_suspended == "Y", 1, 
-                            ifelse(data$depth_range_suspended == "N", 0, data$depth_range_suspended))
+                                       ifelse(data$depth_range_suspended == "N", 0, data$depth_range_suspended))
   
   # Update ""depth_range_floating" scenario
   data$depth_range_floating <- ifelse(data$depth_range_floating == "Y", 1, 
-                            ifelse(data$depth_range_floating == "N", 0, data$depth_range_floating))
+                                      ifelse(data$depth_range_floating == "N", 0, data$depth_range_floating))
   
   # Update "depth_range_intertidal" scenario
   data$depth_range_intertidal <- ifelse(data$depth_range_intertidal == "Y", 1, 
-                            ifelse(data$depth_range_intertidal == "N", 0, data$depth_range_intertidal))
+                                        ifelse(data$depth_range_intertidal == "N", 0, data$depth_range_intertidal))
   
   # Create a "baseline" scenario
   data$baseline <- ifelse(data$bathy_max_mllw < -200, 0, 1)
   
   
-  # Define the output GeoPackage path
-  output_gpkg <- file.path(output_base_path, study_area, "c_submodel_data/constraints/constraints.gpkg")
+  # Raw Geopackage
+  # Define the output raw GeoPackage path
+  output_gpkg <- file.path("C:/Users/Breanna.Xiong/Documents/R Scripts/ak_aoa/data/a_raw_data/extracted_bathymetry_hex_grids_scenarios.gpkg")
+  
+  # Write the layer to the raw output GeoPackage
+  sf::st_write(obj = data, dsn = output_gpkg, layer = layer, append = FALSE)
+  
+  
+  # Submodel Geopackage
+  # Define the output submodel GeoPackage path
+  sub_output_gpkg <- file.path("C:/Users/Breanna.Xiong/Documents/R Scripts/ak_aoa/study_area", study_area, "c_submodel_data/constraints/constraints.gpkg")
+  
+  # Find the corresponding id_num code in the code_dict
+  id_code <- code_dict$id_num[code_index]
   
   # Create the new layer name based on the study area
-  new_layer_name <- paste("ak", code, "CS_054", sep = "_")
-
-                 
+  new_layer_name <- paste("ak", code, id_code, sep = "_")
   
-  # Write the layer to the output GeoPackage
-  sf::st_write(obj = data, dsn = output_gpkg, layer = new_layer_name, append = FALSE)
+  # Write the layer to the submodel output GeoPackage
+  sf::st_write(obj = data, dsn = sub_output_gpkg, layer = new_layer_name, append = FALSE)
 }
-
